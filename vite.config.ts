de7d290c0f6b0286358b0bdb62fedd95ad156daa
Nimith B <nimithbe@gmail.com>
2025-05-02 16:22:12 +0530

    Configure custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/',  // Changed from '/proto-spark-web/'
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-});
+})
